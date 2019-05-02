c8db743a2dce07d8cd64cc373e26d47cfdf630a5
Kim HyunWoo <stories2@naver.com>
2019-05-02 11:07:01 +0900

    mixins 실습

--- styled-awesomeness/src/App.js
+++ styled-awesomeness/src/App.js
@@ -9,10 +9,18 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.88);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`
+
 const Input = styled.input.attrs({
   required: true
 })`
-  border-radius: 5px;
+  border: none;
+  ${awesomeCard}
 `
 
 class App extends Component {
@@ -32,6 +40,7 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
+  /* ${awesomeCard} */
 `
 
 export default App;
