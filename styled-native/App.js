5b0b4acf810f15b4bee5ad1973747ec19eade91a
Kim HyunWoo <stories2@naver.com>
2019-05-02 14:58:14 +0900

    styled-components를 이용한 네이티브 스타일링

--- styled-native/App.js
+++ styled-native/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex:1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+`
+
+const Title = styled.Text`
+  font-weight: 600;
+  font-size: 32px;
+`
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container>
+        <Title>Open up App.js to start working on your app!</Title>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
