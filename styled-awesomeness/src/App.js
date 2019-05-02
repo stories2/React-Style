3f886cbe272860fb8e77838f3b32c183b34c8e58
Kim HyunWoo <stories2@naver.com>
2019-05-02 11:02:31 +0900

    커스텀 attribute 추가

--- styled-awesomeness/src/App.js
+++ styled-awesomeness/src/App.js
@@ -9,15 +9,19 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const Input = styled.input.attrs({
+  required: true
+})`
+  border-radius: 5px;
+`
+
 class App extends Component {
   render() {
     return (
       <Fragment>
         <GlobalStyle />
         <Container>
-          <Button isDanger={true} rotationTime={5}>Hello</Button>
-          <Button isDanger={false}>Hello</Button>
-          <Anchor href="https://google.com">Go to google</Anchor>
+          <Input placeholder="hello"></Input>
         </Container>
       </Fragment>
     )
@@ -28,39 +32,6 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
-`
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color:white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus{
-    outline: none;
-  }
-  background-color: ${props => props.isDanger ? "#e74c3c" : "#2ecc71" }
-  ${props => {
-    if(props.isDanger) {
-      return css `animation: ${rotation} ${props.rotationTime}s linear infinite`
-    }
-  }}
-`
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
 `
 
 export default App;
