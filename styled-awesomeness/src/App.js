d690452760cd3c199f795c4183ee37ea795d851f
Kim HyunWoo <stories2@naver.com>
2019-05-02 09:49:21 +0900

    styled-components 를 이용한 버튼 디자인

--- styled-awesomeness/src/App.js
+++ styled-awesomeness/src/App.js
@@ -5,13 +5,19 @@ import styled from "styled-components";
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Button isDanger={true}></Button>
-        <Button isDanger={false}></Button>
-      </Fragment>
+      <Container>
+        <Button isDanger={true}>Hello</Button>
+        <Button isDanger={false}>Hello</Button>
+      </Container>
     )
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`
 
 const Button = styled.button`
   border-radius: 50px;
@@ -25,6 +31,7 @@ const Button = styled.button`
   &:focus{
     outline: none;
   }
+  background-color: ${props => props.isDanger ? "#e74c3c" : "#2ecc71" }
 `
 
 export default App;
